ed0201abbe3ef611a48e3be7b0508a3f2781bfd9
leopheard <leopheard.misc@gmail.com>
2019-09-19 02:55:21 -0400

    upload

---
+++ resources/media/image.sh
@@ -0,0 +1,35 @@
+convert -resize 512x512 icon.jpg icon.jpg
+
+convert -resize 512x512 icon.jpeg icon.jpg
+
+convert -resize 512x512 icon.png icon.jpg
+
+convert -resize 512x512 logo.jpg icon.jpg
+
+convert -resize 512x512 logo.png icon.jpg
+
+convert -resize 512x512 1.jpg 1.jpg
+
+convert -resize 512x512 1.png 1.png
+
+convert -resize 512x512 1.jpeg 1.jpg
+
+convert -resize 512x512 1.jpg 1.jpg
+
+convert -resize 512x512 1.png 1.jpg
+
+convert -resize 512x512 1.jpeg 1.jpg
+
+convert -resize 512x512 1.jpg 1.jpg
+
+convert -resize 512x512 1.png 1.png
+
+convert -resize 512x512 1.jpeg 1.jpg
+
+convert -resize 1280x720 fanart.jpg fanart.jpg
+
+convert -resize 1280x720 fanart.jpeg fanart.jpg
+
+convert -resize 1280x720 fanart.png fanart.jpg
+
+read
